9d754ba8215dd9df290771db3137cbc2c5011cb3
Steve Haskell <steven.haskell.is@gmail.com>
2019-05-23 08:35:52 -0700

    really a mish mash of concepts but it works. You can swipe left, right or up to change the background color now

--- app/src/main/java/com/jicketyjackindustries/life_assistant/MainActivity.java
+++ app/src/main/java/com/jicketyjackindustries/life_assistant/MainActivity.java
@@ -1,13 +1,22 @@
 package com.jicketyjackindustries.life_assistant;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-
+/*
+This has become an experimentation class for trying out different things in Android Studio
+ */
 public class MainActivity extends AppCompatActivity {
 
     protected View view;
     private boolean isRed;
+    private boolean isBlue;
+    private boolean isGreen;
+
+    /*
+    replaces a constructor, do everything you want to do do on app startup here
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,22 +25,68 @@ public class MainActivity extends AppCompatActivity {
         //variable construction
         view = this.getWindow().getDecorView();
         view.setBackgroundResource(R.color.colorPrimary);
+
+        Context context;
+        context = this.getApplicationContext();
+        view.setOnTouchListener(new OnSwipeTouchListener(context) {
+            @Override
+            public void onSwipeLeft() {
+                flipBackgroundBlue();
+            }
+            @Override
+            public void onSwipeRight(){
+                flipBackgroundGreen();
+            }
+            @Override
+            public void onSwipeUp(){
+                flipBackgroundRed();
+            }
+        });
         isRed=false;
+        isBlue=false;
+        isGreen=false;
 
     }
 
-    public void makeBackgroundRed(View v){
+
+    private void flipBackgroundRed(){
         if(isRed){
             isRed=false;
             view.setBackgroundResource(R.color.colorPrimary);
-
         }
         else{
             isRed=true;
             view.setBackgroundResource(R.color.red);
-
         }
+    }
+    private void flipBackgroundBlue(){
+        if(isBlue){
+            isBlue=false;
+            view.setBackgroundResource(R.color.colorPrimary);
+        }
+        else{
+            isBlue=true;
+            view.setBackgroundResource(R.color.blue);
+        }
+    }
+    private void flipBackgroundGreen(){
+        if(isGreen){
+            isGreen=false;
+            view.setBackgroundResource(R.color.colorPrimary);
+        }
+        else{
+            isGreen=true;
+            view.setBackgroundResource(R.color.green);
+        }
+    }
 
-
+    private void wait(int t){
+        long milli = t*1000;
+        try {
+            Thread.sleep(milli);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
